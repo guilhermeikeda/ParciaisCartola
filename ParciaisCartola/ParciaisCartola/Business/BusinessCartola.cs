aab7f82c9424e06e5466e08cb442b58f8db85852
guilherme.ikeda <guilherme@opus-software.com.br>
2016-10-11 11:23:33 -0300

    Fixed bug with athlets pontuation after closed turn

--- ParciaisCartola/ParciaisCartola/Business/BusinessCartola.cs
+++ ParciaisCartola/ParciaisCartola/Business/BusinessCartola.cs
@@ -2,41 +2,43 @@
 using ParciaisCartola.Models;
 using ParciaisCartola.Models.Response;
 using ParciaisCartola.Services;
+using Refit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ParciaisCartola.Business
 {
-	public class BusinessCartola
-	{
-		private IControllerLigas controllerLigas;
-		private IControllerUsuariosLiga controllerUsuariosLiga;
-		private IControllerAtletas controllerAtletas;
+    public class BusinessCartola
+    {
+        private IControllerLigas controllerLigas;
+        private IControllerUsuariosLiga controllerUsuariosLiga;
+        private IControllerAtletas controllerAtletas;
 
-		public BusinessCartola(IControllerLigas _controllerLigas)
-		{
-			controllerLigas = _controllerLigas;
-		}
+        public BusinessCartola(IControllerLigas _controllerLigas)
+        {
+            controllerLigas = _controllerLigas;
+        }
 
-		public BusinessCartola(IControllerUsuariosLiga _controllerUsuariosLiga)
-		{
-			controllerUsuariosLiga = _controllerUsuariosLiga;
-		}
+        public BusinessCartola(IControllerUsuariosLiga _controllerUsuariosLiga)
+        {
+            controllerUsuariosLiga = _controllerUsuariosLiga;
+        }
 
-		public BusinessCartola(IControllerAtletas _controllerAtletas)
-		{
-			controllerAtletas = _controllerAtletas;
-		}
+        public BusinessCartola(IControllerAtletas _controllerAtletas)
+        {
+            controllerAtletas = _controllerAtletas;
+        }
 
-		public async Task BuscaLiga(string nomeLiga)
-		{
-			try
-			{                
-				List<Liga> ligas = await ServiceRepository.CartolaService.GetLigas(nomeLiga);
-                foreach(var liga in ligas)
+        public async Task BuscaLiga(string nomeLiga)
+        {
+            try
+            {
+                List<Liga> ligas = await ServiceRepository.CartolaService.GetLigas(nomeLiga);
+                foreach (var liga in ligas)
                 {
                     liga.ImagemURI = new UriImageSource()
                     {
@@ -45,68 +47,90 @@ namespace ParciaisCartola.Business
                         CacheValidity = new TimeSpan(5, 0, 0)
                     };
                 }
-				controllerLigas.ExibeListaLigas(ligas);
-				//GloboLogin log = await ServiceRepository.CartolaService.AutenticaUsuarioGlobo("gci_japoneis@hotmail.com", "345288ikeda");
-				//await ServiceRepository.CartolaService.GetUsuariosLiga("opus-software");
-			}
-			catch (Exception e)
-			{
-				System.Diagnostics.Debug.WriteLine(e);
-			}
-		}
+                controllerLigas.ExibeListaLigas(ligas);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+        }
 
-		public async Task BuscaTimesLiga(string slugLiga)
-		{
-			try
-			{
-				List<Time> times = await ServiceRepository.CartolaService.GetTimes(slugLiga);
-                ResponsePontuados atletasPontuados = await ServiceRepository.CartolaService.GetAtletasPontuados();
+        public async Task BuscaTimesLiga(string slugLiga)
+        {
+            try
+            {
+                List<Time> times = await ServiceRepository.CartolaService.GetTimes(slugLiga);
+                ResponsePontuados atletasPontuados = await GetAtletasPontuados();
 
                 foreach (Time time in times)
-				{
-					List<Atleta> atletas = await ServiceRepository.CartolaService.GetAtletasTime(time.Slug);
+                {
+                    List<Atleta> atletas = await ServiceRepository.CartolaService.GetAtletasTime(time.Slug);
                     time.EscudoURI = new UriImageSource()
                     {
                         Uri = new Uri(time.Escudo),
                         CachingEnabled = true,
-
                     };
 
-					double TotalParcial = 0.0;
-					foreach (Atleta atleta in atletas)
-					{
-                        if(atletasPontuados.atletas.ContainsKey(atleta.ID))
+                    double TotalParcial = 0.0;
+
+                    foreach (Atleta atleta in atletas)
+                    {
+                        if (atletasPontuados.atletas.ContainsKey(atleta.ID))
                             TotalParcial += atletasPontuados.atletas[atleta.ID].pontuacao;
                     }
-					time.TotalParcial = string.Format("{0:0.00}", TotalParcial);
-					time.TotalParcialDouble = TotalParcial;
-					await ServiceRepository.CartolaService.InsertTimeCache(time.Slug, time);
-				}
 
-				times = times.OrderByDescending(time => time.TotalParcialDouble).ToList();
+                    time.TotalParcial = string.Format("{0:0.00}", TotalParcial);
+                    time.TotalParcialDouble = TotalParcial;
+                    await ServiceRepository.CartolaService.InsertTimeCache(time.Slug, time);
+                }
 
-				for (var i = 0; i < times.Count; i++)
-				{
-					times[i].PosicaoTabela = (i + 1).ToString() + 'º';
-				}
-				controllerUsuariosLiga.ExibeTimesLiga(times);
-			}
-			catch (Exception e)
-			{
-				System.Diagnostics.Debug.WriteLine(e);
-			}
-		}
+                times = times.OrderByDescending(time => time.TotalParcialDouble).ToList();
 
-		public async Task BuscaAtletasTime(string slugTime)
-		{
-			try
-			{
-				Time time = await ServiceRepository.CartolaService.GetTimeCache(slugTime);
-				controllerAtletas.ExibeTime(time);
+                for (var i = 0; i < times.Count; i++)
+                {
+                    times[i].PosicaoTabela = (i + 1).ToString() + 'º';
+                }
+                controllerUsuariosLiga.ExibeTimesLiga(times);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+        }
 
-				List<Atleta> atletas = await ServiceRepository.CartolaService.GetAtletasTime(slugTime);
+        private async Task<ResponsePontuados> GetAtletasPontuados()
+        {
+            try
+            {
                 ResponsePontuados atletasPontuados = await ServiceRepository.CartolaService.GetAtletasPontuados();
-                foreach(var atleta in atletas)
+                return atletasPontuados;
+            }
+            catch (ApiException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                if (e.StatusCode.Equals(HttpStatusCode.NotFound))
+                {
+                    ResponsePontuados res = new ResponsePontuados()
+                    {
+                        atletas = new Dictionary<int, ResponsePontuados.AtletasPontuados>()
+                    };
+                    return res;
+                }
+                throw e;
+            }
+        }
+
+        public async Task BuscaAtletasTime(string slugTime)
+        {
+            try
+            {
+                Time time = await ServiceRepository.CartolaService.GetTimeCache(slugTime);
+                controllerAtletas.ExibeTime(time);
+
+                List<Atleta> atletas = await ServiceRepository.CartolaService.GetAtletasTime(slugTime);
+                ResponsePontuados atletasPontuados = await GetAtletasPontuados();
+
+                foreach (var atleta in atletas)
                 {
                     if (atletasPontuados.atletas.ContainsKey(atleta.ID))
                         atleta.PontosParcial = atletasPontuados.atletas[atleta.ID].pontuacao.ToString();
@@ -122,11 +146,11 @@ namespace ParciaisCartola.Business
 
                 atletas = atletas.OrderBy(atleta => atleta.PosicaoID).ToList();
                 controllerAtletas.ExibeListaAtletas(atletas);
-			}
-			catch (Exception e)
-			{
-				System.Diagnostics.Debug.WriteLine(e);
-			}
-		}
-	}
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+        }
+    }
 }
